14b9ed5a714e9f2992eb064a1883319406312cc9
Mikołaj Makowski <108890057+miko-mak@users.noreply.github.com>
2024-12-18 20:47:27 +0100

    Update ResetSizeUtil.java

--- src/main/java/pl/mikomak/resizeinator/util/ResetSizeUtil.java
+++ src/main/java/pl/mikomak/resizeinator/util/ResetSizeUtil.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Player;
 public class ResetSizeUtil {
 
     public static void resetSize(final Player player) {
-        // getting the current size of the player by using appropriate GENERIC_SCALE attribute.
+        // getting the current size of the player by using appropriate GENERIC_SCALE attribute
         final AttributeInstance scaleAttribute = player.getAttribute(Attribute.GENERIC_SCALE);
         final double currentSize = scaleAttribute.getBaseValue();
 
